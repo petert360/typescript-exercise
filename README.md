cf2e6a2b28c8dc648a4e0ec5002bcc752198fad7
petert360 <peter.t360@hotmail.com>
2021-08-18 20:32:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-z2ahuh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-z2ahuh)
---
+++ index.ts
@@ -0,0 +1,12 @@
+class Department {
+  name: string;
+  employees: string[] = [];
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
+  }
+}
