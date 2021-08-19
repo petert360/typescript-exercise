671b3d1a411f50eeeb38e067e7488923a6bcc043
petert360 <peter.t360@hotmail.com>
2021-08-19 22:32:21 +0200

    static

--- index.ts
+++ index.ts
@@ -1,24 +1,32 @@
 class Department {
-  public name: string; 
+  public name: string;
   private _employees: string[] = [];
 
+  // static tulajdonság létrehozása
+  static actualYear = 2021;
+
   get listEmployees() {
-    return this._employees
+    return this._employees;
   }
 
   set addToEmployees(employee: string) {
     if (!employee) {
-      throw new Error('Please provide a name')
+      throw new Error('Please provide a name');
     }
-    this.addEmployee(employee)
+    this.addEmployee(employee);
   }
 
-  constructor (n: string) {
+  constructor(n: string) {
     this.name = n;
+    console.log('static tulajdonság: ', Department.actualYear);
+  }
+
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   addEmployee(employee: string) {
-	  this._employees.push(employee)
+    this._employees.push(employee);
   }
 }
 
@@ -29,13 +37,17 @@ const accounting = new Department('Accounting');
 accounting.addEmployee('Jane');
 
 // a getter metódust mint tulajdonságot érjük el
-console.log('Setter előtt: ', accounting.listEmployees)
+console.log('Setter előtt: ', accounting.listEmployees);
 
 // a setter segítségével is beállítunk egy tömb elemet
-accounting.addToEmployees = ''
+accounting.addToEmployees = 'Jack';
 
 // setter után:
-console.log('Setter után: ', accounting.listEmployees)
+console.log('Setter után: ', accounting.listEmployees);
 
 // ha üres stringet adunk át, ami falsy, akkor hibaüzenetet kapunk
 // accounting.addToEmployees = ''
+
+// static metódus hívása és static tulajdonság kiírása
+const employee1 = Department.createEmployee('Joe');
+console.log(employee1, Department.actualYear);
