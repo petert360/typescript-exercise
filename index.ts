5d0041374be28af267077d53cd323e367b0a4c3c
petert360 <73585032+petert360@users.noreply.github.com>
2021-08-20 22:49:01 +0200

    interface functions

--- index.ts
+++ index.ts
@@ -25,3 +25,20 @@ class Person implements Greetable {
 let user1: Greetable;
 user1 = new Person('Peter');
 user1.greet('Hi there, I am');
+
+// type segítségével így hozhatunk létre függvényeket:
+type AddFn = (a: number, b: number) => number;
+let add: AddFn;
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+// interface segítségével is létrehozhatunk függvényeket
+// nevet nem adunk, anonym fv-t hozunk létre
+interface IAddFn {
+  (a: number, b: number): number;
+}
+let addI: IAddFn;
+addI = (n1: number, n2: number) => {
+  return n1 + n2;
+};
