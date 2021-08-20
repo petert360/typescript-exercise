743d58ad22c1f55d9edd1ae9b09cb2df02c9e78f
petert360 <73585032+petert360@users.noreply.github.com>
2021-08-20 22:05:36 +0200

    interface használata

--- index.ts
+++ index.ts
@@ -1,18 +1,36 @@
-interface Person {
+// bármely objektum, amelyet Greetable-kéntk kell kezelni,
+// olyan objektumnak kell lennie, amelynek van egy name talajdonsága
+// és egy greet() metódusa
+
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+// Implmentáljuk a Person osztályba a Greetable interface-t
 
-user1 = {
-  name: 'Peter',
-  age: 30,
+class Person implements Greetable {
+  name: string;
+  age = 30;
+  constructor(n: string) {
+    this.name = n;
+  }
   greet(phrase: string) {
     console.log(phrase + ' ' + this.name);
-  },
-};
+  }
+}
 
+// Most már létrehozható így is az user1 objektum
+let user1: Greetable;
+user1 = new Person('Peter');
 user1.greet('Hi there, I am');
+
+// Vagy így is:
+const user2: Greetable = new Person('Max');
+console.log(user1, user2);
+
+// ugyanakkor az osztállyal történő típusadás is működik:
+let user3: Person;
+user3 = new Person('Someone')
+console.log(user3);
